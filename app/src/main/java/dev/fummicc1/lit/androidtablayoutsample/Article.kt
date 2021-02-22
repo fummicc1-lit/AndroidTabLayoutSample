32e01c347c5662ad80d3a5b97772add0d02036d0
fummicc1 <fumiya.tennis1@gmail.com>
2021-02-23 00:29:37 +0900

    fix: Article data structure. Insert articles in Constants.kt

--- app/src/main/java/dev/fummicc1/lit/androidtablayoutsample/Article.kt
+++ app/src/main/java/dev/fummicc1/lit/androidtablayoutsample/Article.kt
@@ -1,8 +1,5 @@
 package dev.fummicc1.lit.androidtablayoutsample
 
 data class Article(
-    val url: String,
-    val title: String,
-    val imageUrl: String,
-    val description: String
+    val url: String
 )
--- app/src/main/java/dev/fummicc1/lit/androidtablayoutsample/Constants.kt
+++ app/src/main/java/dev/fummicc1/lit/androidtablayoutsample/Constants.kt
@@ -1,7 +1,15 @@
 package dev.fummicc1.lit.androidtablayoutsample
 
 object Constants {
-    val firstArticles: List<Article> = listOf()
-    val secondArticles: List<Article> = listOf()
+    val firstArticles: List<Article> = listOf(
+        Article("https://qiita.com/shikato/items/40ce3955cb7975ad4e2b"),
+        Article("https://itnext.io/android-viewpager2-tablayout-3099aae2f396"),
+        Article("https://developer.android.com/training/appbar/setting-up?hl=ja"),
+    )
+    val secondArticles: List<Article> = listOf(
+        Article("https://qiita.com/pe-ta/items/0ec020c5615e17b1dc48"),
+        Article("https://userweb.mnet.ne.jp/nakama/"),
+        Article("https://developer.android.com/guide/navigation/navigation-swipe-view-2?hl=ja")
+    )
     val thirdArticles: List<Article> = listOf()
 }
