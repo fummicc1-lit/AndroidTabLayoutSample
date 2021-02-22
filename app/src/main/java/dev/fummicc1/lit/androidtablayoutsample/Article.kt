3da5ace901ef325b4a491435f37d356f9babede5
fummicc1 <fumiya.tennis1@gmail.com>
2021-02-23 00:25:29 +0900

    add: Create Article.

---
+++ app/src/main/java/dev/fummicc1/lit/androidtablayoutsample/Article.kt
@@ -0,0 +1,8 @@
+package dev.fummicc1.lit.androidtablayoutsample
+
+data class Article(
+    val url: String,
+    val title: String,
+    val imageUrl: String,
+    val description: String
+)
---
+++ app/src/main/java/dev/fummicc1/lit/androidtablayoutsample/Constants.kt
@@ -0,0 +1,7 @@
+package dev.fummicc1.lit.androidtablayoutsample
+
+object Constants {
+    val firstArticles: List<Article> = listOf()
+    val secondArticles: List<Article> = listOf()
+    val thirdArticles: List<Article> = listOf()
+}
